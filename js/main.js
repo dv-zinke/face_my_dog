6016d44ef6d594bb88cf212b9509f730e99584f0
hj <dv.zinke@gmail.com>
2020-06-09 22:11:05 +0900

    feat : 첫번째 커밋

---
+++ js/main.js
@@ -0,0 +1,85 @@
+var dogData;
+var isLoad;
+
+$(document).ready(function(){
+    getDogData();
+});
+function getDogData(){
+    $.getJSON("assets/data.json", function(json) {
+        isLoad=true;
+        dogData=json;
+    });
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('.image-upload-wrap').hide();
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+            $('.image-title').html(input.files[0].name);
+        };
+        reader.readAsDataURL(input.files[0]);
+        init().then(function(){
+            predict();
+        });
+
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function() {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function() {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
+
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/1US-Bt-Wb/";
+let model, webcam, labelContainer, maxPredictions;
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+    // Convenience function to setup a webcam
+
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    var image = document.getElementById("face-image");
+    const prediction = await model.predict(image, false);
+    prediction.sort(function(a,b){
+        return numberTo(b.probability) - numberTo(a.probability);
+    });
+    for (let i = 0; i < 3; i++) {
+        const classPrediction =
+            "<img src='"+dogData[prediction[i].className.toLowerCase()].url + "'>: " + (prediction[i].probability * 100).toFixed(0);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+}
+
+function numberTo(num){
+    return (num * 100).toFixed(0)
+}
+
